1fd827eb4cff6f8ca023354feba8465325fa506e
Ulises Santana <ulisesantana@gmail.com>
2021-06-19 15:45:54 +0100

    ♻️ split test for better readability

--- src/roundService.test.ts
+++ src/roundService.test.ts
@@ -55,59 +55,6 @@ describe('Round service should', () => {
       expect(rooms[1].participants).toStrictEqual(['Manu', 'Paco'])
     })
 
-    it('with the exact number of participants for splitting into the given room size repeating the rooms after maximum combinations', () => {
-      const roundService = new RoundService()
-      const roomSize = 2
-      const participants = ['Ulises', 'Paco', 'Manu', 'Juan']
-
-      roundService.getRoomsForNextRound(roomSize, participants)
-      roundService.getRoomsForNextRound(roomSize, participants)
-      roundService.getRoomsForNextRound(roomSize, participants)
-
-      let rooms = roundService.getRoomsForNextRound(roomSize, participants)
-      expect(rooms).toHaveLength(2)
-      expect(rooms[0].name).toBe('Room 1')
-      expect(rooms[0].participants).toStrictEqual(['Juan', 'Manu'])
-      expect(rooms[1].name).toBe('Room 2')
-      expect(rooms[1].participants).toStrictEqual(['Paco', 'Ulises'])
-
-      rooms = roundService.getRoomsForNextRound(roomSize, participants)
-      expect(rooms).toHaveLength(2)
-      expect(rooms[0].name).toBe('Room 1')
-      expect(rooms[0].participants).toStrictEqual(['Juan', 'Paco'])
-      expect(rooms[1].name).toBe('Room 2')
-      expect(rooms[1].participants).toStrictEqual(['Manu', 'Ulises'])
-
-      rooms = roundService.getRoomsForNextRound(roomSize, participants)
-      expect(rooms).toHaveLength(2)
-      expect(rooms[0].name).toBe('Room 1')
-      expect(rooms[0].participants).toStrictEqual(['Juan', 'Ulises'])
-      expect(rooms[1].name).toBe('Room 2')
-      expect(rooms[1].participants).toStrictEqual(['Manu', 'Paco'])
-
-      // After no other possible combinations it starts again
-      rooms = roundService.getRoomsForNextRound(roomSize, participants)
-      expect(rooms).toHaveLength(2)
-      expect(rooms[0].name).toBe('Room 1')
-      expect(rooms[0].participants).toStrictEqual(['Juan', 'Manu'])
-      expect(rooms[1].name).toBe('Room 2')
-      expect(rooms[1].participants).toStrictEqual(['Paco', 'Ulises'])
-
-      rooms = roundService.getRoomsForNextRound(roomSize, participants)
-      expect(rooms).toHaveLength(2)
-      expect(rooms[0].name).toBe('Room 1')
-      expect(rooms[0].participants).toStrictEqual(['Juan', 'Paco'])
-      expect(rooms[1].name).toBe('Room 2')
-      expect(rooms[1].participants).toStrictEqual(['Manu', 'Ulises'])
-
-      rooms = roundService.getRoomsForNextRound(roomSize, participants)
-      expect(rooms).toHaveLength(2)
-      expect(rooms[0].name).toBe('Room 1')
-      expect(rooms[0].participants).toStrictEqual(['Juan', 'Ulises'])
-      expect(rooms[1].name).toBe('Room 2')
-      expect(rooms[1].participants).toStrictEqual(['Manu', 'Paco'])
-    })
-
     it('without the exact number of participants for splitting into the given room size', () => {
       const roundService = new RoundService()
       const roomSize = 2
@@ -148,6 +95,59 @@ describe('Round service should', () => {
       expect(rooms[1].name).toBe('Room 2')
       expect(rooms[1].participants).toStrictEqual(['Paco', 'Ulises'])
     })
+  })
+
+  it('retrieve a list of rooms across rounds repeating the rooms after maximum combinations', () => {
+    const roundService = new RoundService()
+    const roomSize = 2
+    const participants = ['Ulises', 'Paco', 'Manu', 'Juan']
+
+    roundService.getRoomsForNextRound(roomSize, participants)
+    roundService.getRoomsForNextRound(roomSize, participants)
+    roundService.getRoomsForNextRound(roomSize, participants)
+
+    let rooms = roundService.getRoomsForNextRound(roomSize, participants)
+    expect(rooms).toHaveLength(2)
+    expect(rooms[0].name).toBe('Room 1')
+    expect(rooms[0].participants).toStrictEqual(['Juan', 'Manu'])
+    expect(rooms[1].name).toBe('Room 2')
+    expect(rooms[1].participants).toStrictEqual(['Paco', 'Ulises'])
+
+    rooms = roundService.getRoomsForNextRound(roomSize, participants)
+    expect(rooms).toHaveLength(2)
+    expect(rooms[0].name).toBe('Room 1')
+    expect(rooms[0].participants).toStrictEqual(['Juan', 'Paco'])
+    expect(rooms[1].name).toBe('Room 2')
+    expect(rooms[1].participants).toStrictEqual(['Manu', 'Ulises'])
+
+    rooms = roundService.getRoomsForNextRound(roomSize, participants)
+    expect(rooms).toHaveLength(2)
+    expect(rooms[0].name).toBe('Room 1')
+    expect(rooms[0].participants).toStrictEqual(['Juan', 'Ulises'])
+    expect(rooms[1].name).toBe('Room 2')
+    expect(rooms[1].participants).toStrictEqual(['Manu', 'Paco'])
+
+    // After no other possible combinations it starts again
+    rooms = roundService.getRoomsForNextRound(roomSize, participants)
+    expect(rooms).toHaveLength(2)
+    expect(rooms[0].name).toBe('Room 1')
+    expect(rooms[0].participants).toStrictEqual(['Juan', 'Manu'])
+    expect(rooms[1].name).toBe('Room 2')
+    expect(rooms[1].participants).toStrictEqual(['Paco', 'Ulises'])
+
+    rooms = roundService.getRoomsForNextRound(roomSize, participants)
+    expect(rooms).toHaveLength(2)
+    expect(rooms[0].name).toBe('Room 1')
+    expect(rooms[0].participants).toStrictEqual(['Juan', 'Paco'])
+    expect(rooms[1].name).toBe('Room 2')
+    expect(rooms[1].participants).toStrictEqual(['Manu', 'Ulises'])
+
+    rooms = roundService.getRoomsForNextRound(roomSize, participants)
+    expect(rooms).toHaveLength(2)
+    expect(rooms[0].name).toBe('Room 1')
+    expect(rooms[0].participants).toStrictEqual(['Juan', 'Ulises'])
+    expect(rooms[1].name).toBe('Room 2')
+    expect(rooms[1].participants).toStrictEqual(['Manu', 'Paco'])
   })
 })
 
